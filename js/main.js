5f042add0a7b9bc4cd9dfcbbff19a7e559c10dcd
kgstwo <kennysmyth@Kenneth-Smiths-MacBook-Pro.local>
2013-11-12 18:44:08 -0500

    test
    test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+function initAll() {
+	// this should call all the oter functions
+	hideShowInfo(); 
+}
 
+initAll();
+
+function hideShowInfo() {
+	var familyButton = $('.family-button,.records-button');
+	var infoHolder = $( '.info-holder' );
+
+	console.log( familyButton )
+
+	familyButton.click( function() {
+		infoHolder.addClass('hidden');
+		var classToHide = $( this ).attr( 'data-showclass' );
+
+		$( '.'+classToHide ).removeClass('hidden');
+
+		return false;
+	});
+}
